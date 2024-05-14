b63f7db1ba4391838a79bc1d7e8c88d7f15c4599
mariosergdc <mariosergdc.webdev@gmail.com>
2024-05-13 20:46:30 -0400

    tailwind media query

---
+++ app/test/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className='space-y-96 border border-red-500 '>
+        <h1>Test page</h1>
+        <div className='w-96 bg-white shadow rounderd border border-red-500'>s</div>
+        <div>s</div>
+        <div>s</div>
+        <div>s</div>
+        <div>s</div>
+    </div>
+  )
+}
+
+export default Test
